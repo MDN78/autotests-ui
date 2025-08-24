eb3a4170785cc21f3221e5d05c8732fd0cef73ad
Dimitri Maksimov <salesakk@gmail.com>
2025-08-24 12:54:00 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,44 @@
-import pytest  # Импортируем pytest
-from playwright.sync_api import sync_playwright, Page, Playwright
+import pytest
+from playwright.sync_api import Page, Playwright, expect
 
 
-@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
+@pytest.fixture
+def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
-        # Передаем страницу для использования в тесте
+    # Передаем страницу для использования в тесте
     yield browser.new_page()
 
     browser.close()
+
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright):
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    expect(email_input).to_be_visible()
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    expect(username_input).to_be_visible()
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    expect(password_input).to_be_visible()
+    password_input.fill('password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_enabled()
+    registration_button.click()
+
+    context.storage_state(path="browser-state.json")
+
+
+@pytest.fixture  # по умолчанию scope=function явное указание не нужно
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state="browser-state.json")
+    return context.new_page()
