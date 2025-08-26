243fa41aba745600a8b117628906d9547a5481f1
Dimitri Maksimov <dmitry-maksimov@yandex.ru>
2025-08-26 23:08:44 +0300

    Знакомство с PageObject. Практика работы с Page Object

---
+++ tests/test_reruns.py
@@ -0,0 +1,22 @@
+import random
+
+import pytest
+
+
+@pytest.mark.flaky(reruns=3, reruns_delay=2)  # Перезапуски реализуются на уровне маркировки flaky
+def test_reruns():
+    assert random.choice([True, False])
+
+
+# tests run 'python -m pytest -k "test_reruns" -s -v'
+
+
+@pytest.mark.flaky(reruns=3, reruns_delay=2)
+class TestReruns:
+    def test_rerun_1(self):
+        assert random.choice([True, False])
+
+    def test_rerun_2(self):
+        assert random.choice([True, False])
+
+# tests run 'python -m pytest -k "TestReruns" -s -v'
