d26fb0e231571e08f2697b7a8897f6da6d503e8f
Dimitri Maksimov <salesakk@gmail.com>
2025-09-18 09:37:49 +0300

    Запуск автотестов на CI

--- tools/playwright/mocks.py
+++ tools/playwright/mocks.py
@@ -1,5 +1,5 @@
 from playwright.sync_api import Page
 
-# Отключаем загрузку статических ресурсов
+# disable downloading static resources
 def mock_static_resources(page: Page):
     page.route("**/*.{ico,png,jpg,webp,mp3,mp4,woff,woff2}", lambda route: route.abort()) # Отменяем загрузку ресурса
